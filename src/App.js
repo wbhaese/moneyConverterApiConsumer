5c5e22208c00baca000d9d42b719412d23f6a44b
wbhaese <wbhaese@gmail.com>
2020-05-02 21:55:47 -0300

    Add money sign

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Conversor moedaA="USD" moedaB="BRL"></Conversor>
-        <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+        <Conversor moedaA="USD" moedaB="BRL" cifrao="$"></Conversor>
+        <Conversor moedaA="BRL" moedaB="USD" cifrao="R$"></Conversor>
         {/* Chama Classe e funções, neste caso com construtor padrão */}
       </div>
     );
